856a2c297877298585367c006a07f6c69a11bdbd
dyadyaJora <ja_massa97@mail.ru>
2016-12-21 02:53:33 +0300

    adding main page

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('Hello World!!!'); //заменить на sendFile()
+  res.sendFile('public/index.html');
 });
 
 
