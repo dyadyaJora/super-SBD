00d50824f4fce7b70cbdf3f463b75002e2694bdc
dyadyaJora <ja_massa97@mail.ru>
2016-12-20 22:27:34 +0300

    bugfix: show error status

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.send('error');
+    res.send('error ' + err.status);
   });
 }
 
