460b51fadb93f97ec5bbc89b1a97e0b50a809b08
TheNech <savchenkov.vk@yandex.ru>
2016-12-21 10:45:30 +0300

    Create company.js

---
+++ company.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+
+var Company = new mongoose.Schema({
+		Brand: {
+	        type: String,
+	        unique: true,
+	        required: true
+    	},
+    	Country: {
+    		type: String;
+    		required: true
+    	},
+    	Year: {
+    		type: Date;
+    	} 
+});
+
+module.exports = mongoose.model('Company', Company);
